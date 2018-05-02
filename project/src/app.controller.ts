f4b09f2d62fe3092df4037972476359616fc8899
RonnyCabrera <ronny-38@hotmail.com>
2018-05-02 15:18:56 -0500

    Aplicaciones Tradicionales

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,9 +1,18 @@
 import { Get, Controller } from '@nestjs/common';
+const fs = require('fs');
+let contador = 0;
 
 @Controller()
 export class AppController {
+
   @Get()
-  root(): string {
-    return 'Hello World!';
+  root() {
+    console.log('Entro al Metodo');
+
+    contador ++;
+    let html = fs.readFileSync(__dirname + '/html/index.html', 'utf8');
+    html = html.replace('{{variable}}', contador);
+    //console.log('Termino el Metodo');
+    return html;
   }
 }
--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [], //importar otros modulos
   controllers: [AppController],
   components: [],
 })

8e6f090310552eb683e8f88b868db80b497a949b
RonnyCabrera <ronny-38@hotmail.com>
2018-05-09 14:28:27 -0500

    Aumentar el Controlador Creado

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import {AppPeliculaController} from "./app.pelicula-controller";
+import {ParametrosController} from "./parametros.controller";
 
 @Module({
   imports: [], //importar otros modulos
-  controllers: [AppController, AppPeliculaController],
+  controllers: [AppController, AppPeliculaController, ParametrosController],
   components: [],
 })
 export class AppModule {}
